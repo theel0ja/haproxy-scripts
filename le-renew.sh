2102c653c968ee8105186ce50c818e8198aed421
Elias Ojala <git@eliasojala.me>
2018-07-29 22:57:09 +0200

    do not hardcode

--- le-renew.sh
+++ le-renew.sh
@@ -1,7 +1,9 @@
+HAPROXY_FOLDER=$PWD
+
 docker run -it --rm --name certbot-renew \
     -v "letsencrypt-etc:/etc/letsencrypt" \
     -v "letsencrypt-var-lib:/var/lib/letsencrypt" \
-    -v "/app/haproxy/secrets:/app/haproxy/secrets" \
+    -v "${HAPROXY_FOLDER}/secrets:/app/haproxy/secrets" \
     -v "haproxy-certs:/certs" \
-    -v "/app/haproxy/scripts/le-combiner:/scripts/le-combiner" \
+    -v "${HAPROXY_FOLDER}/scripts/le-combiner:/scripts/le-combiner" \
     certbot/dns-cloudflare renew --renew-hook "/scripts/le-combiner/le-combiner.sh"
