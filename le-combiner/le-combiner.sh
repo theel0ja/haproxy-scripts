986b02abc1910c3e17f7c1fc926746be31e73f0c
Elias Ojala <git@eliasojala.me>
2019-05-14 17:26:46 +0000

    Update

--- le-combiner/le-combiner.sh
+++ le-combiner/le-combiner.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
+# https://askubuntu.com/a/15856
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
+FOLDER=/etc/haproxy/certs
+
 # Combine function
 combine() {
     # move to the correct let's encrypt directory
     cd /etc/letsencrypt/live/$SITE
 
+    COMBINED_FILE=$FOLDER/$SITE.pem
+
     # cat files to make combined .pem for haproxy
-    cat fullchain.pem > /certs/$SITE.pem
-    echo "" >> /certs/$SITE.pem
-    cat privkey.pem >> /certs/$SITE.pem
+    cat fullchain.pem > $COMBINED_FILE
+    echo "" >> $COMBINED_FILE
+    cat privkey.pem >> $COMBINED_FILE
+
+    chmod 600 $COMBINED_FILE
 }
 
 
